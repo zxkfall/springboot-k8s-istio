357027cf213a3151616f7e38958738e9840aa040
xingkun.zhang <1475795322@qq.com>
2023-12-28 18:26:17 +0800

    feat: first api test

--- src/test/java/com/flywinter/demo/DemoApplicationTests.java
+++ src/test/java/com/flywinter/demo/DemoApplicationTests.java
@@ -1,13 +1,28 @@
 package com.flywinter.demo;
 
+import com.flywinter.demo.controller.IndexController;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@SpringBootTest
+import java.util.Date;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(IndexController.class)
 class DemoApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Welcome to SpringBoot with Kubernetes and Istio %s".formatted(new Date())));
 	}
 
 }
